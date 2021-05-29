e2655825248959ef0804598be62dc8851b6e8416
Olek Myron <myrondell@gmail.com>
2021-05-29 08:04:38 +0300

    Updates

--- src.6d1d5917.js
+++ src.6d1d5917.js
@@ -5,4 +5,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./js/theme-switch");
 },{"./sass/main.scss":"clu1","./js/theme-switch":"/JIh"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-js-hw-10-food-service/src.b1908e76.js.map
+//# sourceMappingURL=/goit-js-hw-10-food-service/src.6d1d5917.js.map
